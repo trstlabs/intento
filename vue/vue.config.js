d9a3974e9290d6c70380ce267686a208e3970d75
Daniel Dijkstra <danieljdijkstra@gmail.com>
2021-03-15 19:15:29 +0100

    really split vendor

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -10,29 +10,27 @@ module.exports = {
 
     ],
     optimization: {
+      removeAvailableModules: true,
       splitChunks: {
         chunks: 'async',
         minSize: 20000,
-
+      
         maxSize: 0,
         minChunks: 1,
         maxAsyncRequests: 30,
         maxInitialRequests: 30,
         enforceSizeThreshold: 50000,
         cacheGroups: {
-          defaultVendors: {
+          vendor: {
             test: /[\\/]node_modules[\\/]/,
-            priority: -10,
-            reuseExistingChunk: true,
-          },
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+            
           },
         },
       },
     },
   },
-
 };
