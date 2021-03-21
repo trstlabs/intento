2c09c2a1bac9b7f996afece66bf3891f8ee19662
Daniel Dijkstra <danieljdijkstra@gmail.com>
2021-03-21 11:33:12 +0100

    messages sign in first

--- vue/src/components/MessageBox.vue
+++ vue/src/components/MessageBox.vue
@@ -57,7 +57,8 @@
       </button>
       <button class="button-cancel" @click="removeRoomId = null">Cancel</button>
     </form>
-    <p v-if="!this.currentUserId" class="pa-8 text-center">  Loading messages... <button>Refresh</button> </p>
+    <p v-if="!this.currentUserId && this.$store.state.account.address" class="pa-8 text-center">  Loading messages... <button>Refresh</button> </p>
+    <p v-if="!this.$store.state.account.address" class="pa-8 text-center">  Sign in first <button>Refresh</button> </p>
     <chat-window v-if="this.currentUserId"
       :styles="$vuetify.theme.dark == true ? stylesdark : styles "
       :current-user-id="this.currentUserId"
