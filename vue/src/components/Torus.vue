7f818c686a61a50c3de5e1baa9aed5d022119140
Daniel Dijkstra <danieljdijkstra@gmail.com>
2021-03-22 20:07:09 +0100

    sign in

--- vue/src/components/Torus.vue
+++ vue/src/components/Torus.vue
@@ -62,6 +62,46 @@ export default {
   created(){
     
     if (this.privkey) {
+      //if this doesnt work for returning user, put emailRef outside of it and  try signing in right away. First test if auth is able to handle handle it from its localstorage
+
+      // Confirm the link is a sign-in with email link.
+if (auth.isSignInWithEmailLink(window.location.href)) {
+  // Additional state parameters can also be passed via URL.
+  // This can be used to continue the user's intended action before triggering
+  // the sign-in operation.
+  // Get the email if available. This should be available if the user completes
+  // the flow on the same device where they started it.
+  var email = window.localStorage.getItem('emailForSignIn');
+  var emailRef = window.localStorage.getItem('emailRef');
+
+  if (!email) {
+    // User opened the link on a different device. To prevent session fixation
+    // attacks, ask the user to provide the associated email again. For example:
+    email = window.prompt('Please provide your email for confirmation');
+  }
+
+  if (emailRef) {
+     auth.signInWithEmailLink(email, emailRef)
+  }
+  // The client SDK will parse the code from the link for you.
+  auth.signInWithEmailLink(email, window.location.href)
+    .then((result) => {
+      // Clear email from storage.
+      //window.localStorage.removeItem('emailForSignIn');
+     window.localStorage.setItem('emailRef', window.location.href);
+      this.$store.dispatch("set", "user", result.user)
+      console.log(this.$store.state.user)
+      // You can access the new user via result.user
+      // Additional user info profile not available via:
+      // result.additionalUserInfo.profile == null
+      // You can check if the user is new or existing:
+      // result.additionalUserInfo.isNewUser
+    })
+    .catch((error) => {
+      // Some error occurred, you can inspect the code: error.code
+      // Common errors could be invalid email and invalid or expired OTPs.
+    });
+}   
           this.torusSignIn(this.privkey)}
   },
   computed: {
@@ -185,36 +225,7 @@ export default {
     // ...
   });
 
-// Confirm the link is a sign-in with email link.
-if (auth.isSignInWithEmailLink(window.location.href)) {
-  // Additional state parameters can also be passed via URL.
-  // This can be used to continue the user's intended action before triggering
-  // the sign-in operation.
-  // Get the email if available. This should be available if the user completes
-  // the flow on the same device where they started it.
-  var email = window.localStorage.getItem('emailForSignIn');
-  if (!email) {
-    // User opened the link on a different device. To prevent session fixation
-    // attacks, ask the user to provide the associated email again. For example:
-    email = window.prompt('Please provide your email for confirmation');
-  }
-  // The client SDK will parse the code from the link for you.
-  auth.signInWithEmailLink(email, window.location.href)
-    .then((result) => {
-      // Clear email from storage.
-      window.localStorage.removeItem('emailForSignIn');
-      this.$store.dispatch("set", "user", result.user)
-      // You can access the new user via result.user
-      // Additional user info profile not available via:
-      // result.additionalUserInfo.profile == null
-      // You can check if the user is new or existing:
-      // result.additionalUserInfo.isNewUser
-    })
-    .catch((error) => {
-      // Some error occurred, you can inspect the code: error.code
-      // Common errors could be invalid email and invalid or expired OTPs.
-    });
-}   
+
       } catch (error) {
         this.loading = false
         console.error(error, "caught");
