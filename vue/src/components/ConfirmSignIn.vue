cbb6906e64163f0f66dd792c34ab46b04337157f
Daniel Dijkstra <danieljdijkstra@gmail.com>
2021-04-22 14:45:28 +0200

    buy item layout change, confirm msg, walletcoins

--- vue/src/components/ConfirmSignIn.vue
+++ vue/src/components/ConfirmSignIn.vue
@@ -5,7 +5,7 @@
      <v-btn @click="sent = true, sendMail()"> Send</v-btn>  </div>
     <div v-else>
       <v-alert  
-  type="success"  class="caption"
+  type="info"  class="caption"
 >
 Confirm this sign-in once, by clicking the link sent to <span v-if="this.email"> {{email}} </span> <span v-else> your Google account's email </span >, on this device.
 </v-alert> </div>
