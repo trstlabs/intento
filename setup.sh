4fcc6721a82da66063995de19b70502f55c45725
daniel <danieljdijkstra@gmail.om>
2021-06-21 14:49:30 +0200

    cleanup and minor fixes

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
 
-//reinstalling
-make deb
-sudo apt remove trustpriceprotocol -y
-sudo apt install ./trustpriceprotocol_0.6.1.0-14-gd1f4289_amd64.deb
-./init.sh
+tppd tx compute store ./independent_price_secret_contract/contract.wasm.gz --from faucet --fees 500000tpp --gas 2000000 --chain-id tpp --keyring-backend test -y
+tppd tx compute instantiate 1 '{"estimationcount": "5"}' --label hahalololol --from faucet --fees 500000tpp --gas 2000000 --chain-id tpp --keyring-backend test -y
 
-tppd init-bootstrap
-PUBLIC_KEY=$(tppd parse attestation_cert.der 2> /dev/null | cut -c 3-)
-echo $PUBLIC_KEY
+tppd tx compute execute cosmos18vd8fpwxzck93qlwghaj6arh4p7c5n89uzcee5 '{"send": {"amount": "46"}}' --from user1 --gas 500000 -y --fees 530tpp --chain-id tpp --keyring-backend test -y
+ 
