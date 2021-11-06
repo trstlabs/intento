714312ee73661d4b3dc3e104931a08a80c20a63a
Trustless Hub <73590614+TRST-labs@users.noreply.github.com>
2021-11-06 15:58:16 +0100

    Delete vue directory

--- vue/src/store/generated/danieljdd/trst/danieljdd.trst.trst/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { MsgUpdateBuyer } from "./module/types/item/buyer";
-import { Estimator } from "./module/types/item/estimator";
-import { Item } from "./module/types/item/item";
-import { MsgUpdateItem } from "./module/types/item/item";
-import { Params } from "./module/types/item/item";
-export { MsgUpdateBuyer, Estimator, Item, MsgUpdateItem, Params };
-declare const _default;
-export default _default;
