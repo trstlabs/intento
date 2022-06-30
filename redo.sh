70e2d53645985a1f27fb9358bdec4e0ef8838d38
trst <danieljdijkstra@gmail.com>
2022-06-30 08:23:25 +0200

    minor fixes

--- redo.sh
+++ redo.sh
@@ -1,5 +1,5 @@
 sudo apt remove trustless-hub-node -y
 make clean-files
-sudo apt install ./trustless-hub-node_0.7.5-22-g70baf50_amd64.deb -y
+sudo apt install ./trustless-hub-node_0.7.7_amd64.deb -y
 
 ./init.sh
