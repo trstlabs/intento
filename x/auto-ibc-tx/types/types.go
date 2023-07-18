2a74662ffbecd35d530ce24ef88d5126ccd3c2e6
danieljdd <73590614+danieljdd@users.noreply.github.com>
2023-07-18 15:15:29 +0000

    fix: use UnpackAny for getting sdkmsg

--- x/auto-ibc-tx/types/types.go
+++ x/auto-ibc-tx/types/types.go
@@ -1,21 +1,23 @@
 package types
 
 import (
+	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var Denom = "utrst"
+var ParseICAValue = "ICA_ADDR"
 
-// GetTxMsgs fetches cached any messages
-func (autoTxInfo AutoTxInfo) GetTxMsgs() []sdk.Msg {
-	var sdkMsgs []sdk.Msg
+// GetTxMsgs unpacks sdk messages from any messages
+func (autoTxInfo AutoTxInfo) GetTxMsgs(unpacker types.AnyUnpacker) (sdkMsgs []sdk.Msg) {
+
 	for _, message := range autoTxInfo.Msgs {
-		sdkMsg, ok := message.GetCachedValue().(sdk.Msg)
-		if !ok {
+		var sdkMsg sdk.Msg
+		err := unpacker.UnpackAny(message, sdkMsg)
+		if err != nil {
 			return nil
 		}
 		sdkMsgs = append(sdkMsgs, sdkMsg)
 	}
-
 	return sdkMsgs
 }
