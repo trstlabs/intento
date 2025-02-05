717edc120c87a7c7d3c84672a15023a5f377db22
danieljdd <danieljdijkstra@gmail.com>
2025-02-05 11:35:27 +0100

    clean up config

--- cmd/intentod/cmd/config/config.go
+++ cmd/intentod/cmd/config/config.go
@@ -34,24 +34,11 @@ const (
 )
 
 func SetupConfig() {
-	// config := sdk.GetConfig()
-	// SetBech32Prefixes(config)
-	// SetBip44CoinType(config)
-	// SetAddressPrefixes(config)
-	// config.Seal()
 	cfg := sdk.GetConfig()
 	cfg.SetBech32PrefixForAccount(appparams.Bech32PrefixAccAddr, appparams.Bech32PrefixAccPub)
 	cfg.SetBech32PrefixForValidator(appparams.Bech32PrefixValAddr, appparams.Bech32PrefixValPub)
 	cfg.SetBech32PrefixForConsensusNode(appparams.Bech32PrefixConsAddr, appparams.Bech32PrefixConsPub)
-	//cfg.SetAddressVerifier(wasmtypes.VerifyAddressLen())
 	cfg.Seal()
-}
-
-// SetBech32Prefixes sets the global prefixes to be used when serializing addresses and public keys to Bech32 strings.
-func SetBech32Prefixes(config *sdk.Config) {
-	config.SetBech32PrefixForAccount(Bech32PrefixAccAddr, Bech32PrefixAccPub)
-	config.SetBech32PrefixForValidator(Bech32PrefixValAddr, Bech32PrefixValPub)
-	config.SetBech32PrefixForConsensusNode(Bech32PrefixConsAddr, Bech32PrefixConsPub)
 }
 
 // SetBip44CoinType sets the global coin type to be used in hierarchical deterministic wallets.
