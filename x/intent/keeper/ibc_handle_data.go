2ccd356efa665c1c19102680a3a8d0835e1f0b1d
danieljdd <danieljdijkstra@gmail.com>
2025-06-07 10:19:24 +0200

    feat: add elys message type routing

--- x/intent/keeper/ibc_handle_data.go
+++ x/intent/keeper/ibc_handle_data.go
@@ -2,128 +2,23 @@ package keeper
 
 import (
 	errorsmod "cosmossdk.io/errors"
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	authztypes "github.com/cosmos/cosmos-sdk/x/authz"
-	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
-	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
-	proto "github.com/cosmos/gogoproto/proto"
-	osmosisgammv1beta1 "github.com/trstlabs/intento/x/intent/msg_registry/osmosis/gamm/v1beta1"
+	"github.com/cosmos/gogoproto/proto"
+
+	"github.com/trstlabs/intento/x/intent/msg_registry"
 	"github.com/trstlabs/intento/x/intent/types"
 )
 
 func handleMsgData(msgData *sdk.MsgData) (proto.Message, int, error) {
-	// fmt.Printf("handling data for typeurl: %v and data: %v\n", msgData.MsgType, msgData.Data)
-
-	var msgResponse proto.Message
-	var rewardType int
-	switch msgData.MsgType {
-
-	// authz
-	case sdk.MsgTypeURL(&authztypes.MsgExec{}):
-		msgResponse = &authztypes.MsgExecResponse{}
-		rewardType = types.KeyFlowIncentiveForAuthzTx
-
-	// sdk
-	case sdk.MsgTypeURL(&banktypes.MsgSend{}):
-		msgResponse = &banktypes.MsgSendResponse{}
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	case sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}):
-		msgResponse = &stakingtypes.MsgDelegateResponse{}
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	case sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}):
-		msgResponse = &stakingtypes.MsgUndelegateResponse{}
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	case sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}):
-		msgResponse = &distrtypes.MsgWithdrawDelegatorRewardResponse{}
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	// wasm
-	case sdk.MsgTypeURL(&wasmtypes.MsgExecuteContract{}):
-		msgResponse = &wasmtypes.MsgExecuteContractResponse{}
-		rewardType = types.KeyFlowIncentiveForWasmTx
-
-	case sdk.MsgTypeURL(&wasmtypes.MsgInstantiateContract{}):
-		msgResponse = &wasmtypes.MsgInstantiateContractResponse{}
-		rewardType = types.KeyFlowIncentiveForWasmTx
-
-	// osmo
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgSwapExactAmountIn{}):
-		msgResponse = &osmosisgammv1beta1.MsgSwapExactAmountInResponse{}
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgSwapExactAmountOut{}):
-		msgResponse = &osmosisgammv1beta1.MsgSwapExactAmountOutResponse{}
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgJoinPool{}):
-		msgResponse = &osmosisgammv1beta1.MsgJoinPoolResponse{}
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgExitPool{}):
-		msgResponse = &osmosisgammv1beta1.MsgExitPoolResponse{}
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	default:
-		return nil, -1, nil
+	entry, ok := msg_registry.MsgRegistry[msgData.MsgType]
+	if !ok {
+		return nil, -1, nil // unknown msg type
 	}
 
+	msgResponse := entry.NewResponse()
 	if err := proto.Unmarshal(msgData.Data, msgResponse); err != nil {
 		return nil, -1, errorsmod.Wrapf(types.ErrJSONUnmarshal, "cannot unmarshal response message: %s", err.Error())
 	}
 
-	return msgResponse, rewardType, nil
-}
-
-func getMsgRewardType(typeUrl string) int {
-	var rewardType int
-
-	switch typeUrl {
-
-	// authz
-	case sdk.MsgTypeURL(&authztypes.MsgExec{}):
-		rewardType = types.KeyFlowIncentiveForAuthzTx
-
-	// sdk
-	case sdk.MsgTypeURL(&banktypes.MsgSend{}):
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	case sdk.MsgTypeURL(&stakingtypes.MsgDelegate{}):
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	case sdk.MsgTypeURL(&stakingtypes.MsgUndelegate{}):
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	case sdk.MsgTypeURL(&distrtypes.MsgWithdrawDelegatorReward{}):
-		rewardType = types.KeyFlowIncentiveForSDKTx
-
-	// wasm
-	case sdk.MsgTypeURL(&wasmtypes.MsgExecuteContract{}):
-		rewardType = types.KeyFlowIncentiveForWasmTx
-
-	case sdk.MsgTypeURL(&wasmtypes.MsgInstantiateContract{}):
-		rewardType = types.KeyFlowIncentiveForWasmTx
-
-	// osmo
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgSwapExactAmountIn{}):
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgSwapExactAmountOut{}):
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgJoinPool{}):
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	case sdk.MsgTypeURL(&osmosisgammv1beta1.MsgExitPool{}):
-		rewardType = types.KeyFlowIncentiveForOsmoTx
-
-	default:
-		return -1
-	}
-
-	return rewardType
+	return msgResponse, entry.RewardType, nil
 }
--- x/intent/msg_registry/codec.go
+++ x/intent/msg_registry/codec.go
@@ -3,6 +3,7 @@ package msg_registry
 import (
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	proto "github.com/cosmos/gogoproto/proto"
 	elysamm "github.com/trstlabs/intento/x/intent/msg_registry/elys/amm"
 	elyscommitment "github.com/trstlabs/intento/x/intent/msg_registry/elys/commitment"
 	elysestaking "github.com/trstlabs/intento/x/intent/msg_registry/elys/estaking"
@@ -12,6 +13,7 @@ import (
 	elysstablestake "github.com/trstlabs/intento/x/intent/msg_registry/elys/stablestake"
 	elystradeshield "github.com/trstlabs/intento/x/intent/msg_registry/elys/tradeshield"
 	osmosisgammv1beta1 "github.com/trstlabs/intento/x/intent/msg_registry/osmosis/gamm/v1beta1"
+	types "github.com/trstlabs/intento/x/intent/types"
 )
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
@@ -103,3 +105,98 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 }
 
 type RawContractMessage []byte
+
+var MsgRegistry = map[string]struct {
+	NewResponse func() proto.Message
+	RewardType  int
+}{
+	// Osmosis Gamm
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgExitPool{}):                {func() proto.Message { return &osmosisgammv1beta1.MsgExitPoolResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgExitSwapExternAmountOut{}): {func() proto.Message { return &osmosisgammv1beta1.MsgExitSwapExternAmountOutResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgExitSwapShareAmountIn{}):   {func() proto.Message { return &osmosisgammv1beta1.MsgExitSwapShareAmountInResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgJoinPool{}):                {func() proto.Message { return &osmosisgammv1beta1.MsgJoinPoolResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgJoinSwapExternAmountIn{}):  {func() proto.Message { return &osmosisgammv1beta1.MsgJoinSwapExternAmountInResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgJoinSwapShareAmountOut{}):  {func() proto.Message { return &osmosisgammv1beta1.MsgJoinSwapShareAmountOutResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgSwapExactAmountIn{}):       {func() proto.Message { return &osmosisgammv1beta1.MsgSwapExactAmountInResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+	sdk.MsgTypeURL(&osmosisgammv1beta1.MsgSwapExactAmountOut{}):      {func() proto.Message { return &osmosisgammv1beta1.MsgSwapExactAmountOutResponse{} }, types.KeyFlowIncentiveForOsmoTx},
+
+	// Elys AMM
+	sdk.MsgTypeURL(&elysamm.MsgCreatePool{}):                    {func() proto.Message { return &elysamm.MsgCreatePoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgJoinPool{}):                      {func() proto.Message { return &elysamm.MsgJoinPoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgExitPool{}):                      {func() proto.Message { return &elysamm.MsgExitPoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgUpFrontSwapExactAmountIn{}):      {func() proto.Message { return &elysamm.MsgUpFrontSwapExactAmountInResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgSwapExactAmountIn{}):             {func() proto.Message { return &elysamm.MsgSwapExactAmountInResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgSwapExactAmountOut{}):            {func() proto.Message { return &elysamm.MsgSwapExactAmountOutResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgSwapByDenom{}):                   {func() proto.Message { return &elysamm.MsgSwapByDenomResponse{} }, -1},
+	sdk.MsgTypeURL(&elysamm.MsgFeedMultipleExternalLiquidity{}): {func() proto.Message { return &elysamm.MsgFeedMultipleExternalLiquidityResponse{} }, -1},
+
+	// Elys TradeShield
+	sdk.MsgTypeURL(&elystradeshield.MsgCreateSpotOrder{}):           {func() proto.Message { return &elystradeshield.MsgCreateSpotOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgUpdateSpotOrder{}):           {func() proto.Message { return &elystradeshield.MsgUpdateSpotOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgCancelSpotOrder{}):           {func() proto.Message { return &elystradeshield.MsgCancelSpotOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgCancelSpotOrders{}):          {func() proto.Message { return &elystradeshield.MsgCancelSpotOrdersResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgCreatePerpetualOpenOrder{}):  {func() proto.Message { return &elystradeshield.MsgCreatePerpetualOpenOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgCreatePerpetualCloseOrder{}): {func() proto.Message { return &elystradeshield.MsgCreatePerpetualCloseOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgUpdatePerpetualOrder{}):      {func() proto.Message { return &elystradeshield.MsgUpdatePerpetualOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgCancelPerpetualOrder{}):      {func() proto.Message { return &elystradeshield.MsgCancelPerpetualOrderResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgCancelPerpetualOrders{}):     {func() proto.Message { return &elystradeshield.MsgCancelPerpetualOrdersResponse{} }, -1},
+	sdk.MsgTypeURL(&elystradeshield.MsgExecuteOrders{}):             {func() proto.Message { return &elystradeshield.MsgExecuteOrdersResponse{} }, -1},
+
+	// Elys Perpetual
+	sdk.MsgTypeURL(&elysperpetual.MsgOpen{}):                     {func() proto.Message { return &elysperpetual.MsgOpenResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgClose{}):                    {func() proto.Message { return &elysperpetual.MsgCloseResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgUpdateParams{}):             {func() proto.Message { return &elysperpetual.MsgUpdateParamsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgWhitelist{}):                {func() proto.Message { return &elysperpetual.MsgWhitelistResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgDewhitelist{}):              {func() proto.Message { return &elysperpetual.MsgDewhitelistResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgUpdateStopLoss{}):           {func() proto.Message { return &elysperpetual.MsgUpdateStopLossResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgClosePositions{}):           {func() proto.Message { return &elysperpetual.MsgClosePositionsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgUpdateTakeProfitPrice{}):    {func() proto.Message { return &elysperpetual.MsgUpdateTakeProfitPriceResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgUpdateMaxLeverageForPool{}): {func() proto.Message { return &elysperpetual.MsgUpdateMaxLeverageForPoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysperpetual.MsgUpdateEnabledPools{}):       {func() proto.Message { return &elysperpetual.MsgUpdateEnabledPoolsResponse{} }, -1},
+
+	// Elys eStaking
+	sdk.MsgTypeURL(&elysestaking.MsgWithdrawReward{}):             {func() proto.Message { return &elysestaking.MsgWithdrawRewardResponse{} }, -1},
+	sdk.MsgTypeURL(&elysestaking.MsgWithdrawElysStakingRewards{}): {func() proto.Message { return &elysestaking.MsgWithdrawElysStakingRewardsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysestaking.MsgWithdrawAllRewards{}):         {func() proto.Message { return &elysestaking.MsgWithdrawAllRewardsResponse{} }, -1},
+
+	// Elys Leverage LP
+	sdk.MsgTypeURL(&elysleveragelp.MsgOpen{}):               {func() proto.Message { return &elysleveragelp.MsgOpenResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgClose{}):              {func() proto.Message { return &elysleveragelp.MsgCloseResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgClaimRewards{}):       {func() proto.Message { return &elysleveragelp.MsgClaimRewardsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgUpdateParams{}):       {func() proto.Message { return &elysleveragelp.MsgUpdateParamsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgAddPool{}):            {func() proto.Message { return &elysleveragelp.MsgAddPoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgRemovePool{}):         {func() proto.Message { return &elysleveragelp.MsgRemovePoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgWhitelist{}):          {func() proto.Message { return &elysleveragelp.MsgWhitelistResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgDewhitelist{}):        {func() proto.Message { return &elysleveragelp.MsgDewhitelistResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgUpdateStopLoss{}):     {func() proto.Message { return &elysleveragelp.MsgUpdateStopLossResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgClosePositions{}):     {func() proto.Message { return &elysleveragelp.MsgClosePositionsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgUpdatePool{}):         {func() proto.Message { return &elysleveragelp.MsgUpdatePoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysleveragelp.MsgUpdateEnabledPools{}): {func() proto.Message { return &elysleveragelp.MsgUpdateEnabledPoolsResponse{} }, -1},
+
+	// Elys MasterChef
+	sdk.MsgTypeURL(&elysmasterchef.MsgAddExternalRewardDenom{}): {func() proto.Message { return &elysmasterchef.MsgAddExternalRewardDenomResponse{} }, -1},
+	sdk.MsgTypeURL(&elysmasterchef.MsgAddExternalIncentive{}):   {func() proto.Message { return &elysmasterchef.MsgAddExternalIncentiveResponse{} }, -1},
+	sdk.MsgTypeURL(&elysmasterchef.MsgUpdatePoolMultipliers{}):  {func() proto.Message { return &elysmasterchef.MsgUpdatePoolMultipliersResponse{} }, -1},
+	sdk.MsgTypeURL(&elysmasterchef.MsgClaimRewards{}):           {func() proto.Message { return &elysmasterchef.MsgClaimRewardsResponse{} }, -1},
+	sdk.MsgTypeURL(&elysmasterchef.MsgTogglePoolEdenRewards{}):  {func() proto.Message { return &elysmasterchef.MsgTogglePoolEdenRewardsResponse{} }, -1},
+
+	// Elys StableStake
+	sdk.MsgTypeURL(&elysstablestake.MsgBond{}):       {func() proto.Message { return &elysstablestake.MsgBondResponse{} }, -1},
+	sdk.MsgTypeURL(&elysstablestake.MsgUnbond{}):     {func() proto.Message { return &elysstablestake.MsgUnbondResponse{} }, -1},
+	sdk.MsgTypeURL(&elysstablestake.MsgAddPool{}):    {func() proto.Message { return &elysstablestake.MsgAddPoolResponse{} }, -1},
+	sdk.MsgTypeURL(&elysstablestake.MsgUpdatePool{}): {func() proto.Message { return &elysstablestake.MsgUpdatePoolResponse{} }, -1},
+
+	// Elys Commitment
+	sdk.MsgTypeURL(&elyscommitment.MsgCommitClaimedRewards{}): {func() proto.Message { return &elyscommitment.MsgCommitClaimedRewardsResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgUncommitTokens{}):       {func() proto.Message { return &elyscommitment.MsgUncommitTokensResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgVest{}):                 {func() proto.Message { return &elyscommitment.MsgVestResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgCancelVest{}):           {func() proto.Message { return &elyscommitment.MsgCancelVestResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgClaimVesting{}):         {func() proto.Message { return &elyscommitment.MsgClaimVestingResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgUpdateEnableVestNow{}):  {func() proto.Message { return &elyscommitment.MsgUpdateEnableVestNowResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgVestNow{}):              {func() proto.Message { return &elyscommitment.MsgVestNowResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgVestLiquid{}):           {func() proto.Message { return &elyscommitment.MsgVestLiquidResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgStake{}):                {func() proto.Message { return &elyscommitment.MsgStakeResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgUnstake{}):              {func() proto.Message { return &elyscommitment.MsgUnstakeResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgClaimKol{}):             {func() proto.Message { return &elyscommitment.MsgClaimKolResponse{} }, -1},
+	sdk.MsgTypeURL(&elyscommitment.MsgClaimRewardProgram{}):   {func() proto.Message { return &elyscommitment.MsgClaimRewardProgramResponse{} }, -1},
+}
